d4832b5995338efb6f5097b7ffc3312e193f3a98
Seth Forte <sethforte1@gmail.com>
2025-04-15 16:46:11 -0500

    Completeing Exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            string personName = "John Doe";
+            int personAge = 11;
+            char firstCharacter = 'J';
+            bool isPerson = true;
+            double personHeight = 1.75;
+            decimal personWeight = 70.5m;
+            Console.WriteLine($"My name is: {personName} and I am {personAge} years old. The first character in my name is: {firstCharacter}. Am I a person? {isPerson}, why yes I am. I am {personHeight} tall and I weigh {personWeight}.");
         }
     }
 }
